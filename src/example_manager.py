9dbc12844a861490b378534503379cbcffaf6093
Zach Moore <zachdmoore@gmail.com>
2020-02-20 16:46:50 -0500

    Added Manual Input to our experience manager so you don't have to retype the commands each time

--- src/example_manager.py
+++ src/example_manager.py
@@ -1,3 +1,5 @@
+import pyautogui
+
 #Interprets response from Camelot when sending command to it
 def check_for_success(command):
     while(True):
@@ -9,11 +11,15 @@ def check_for_success(command):
         elif(i.startswith('error')):
             return False
 
+def talk_action(person):
+    command = 'SetDialog(What do you want? [Nothing | Nothing])'
+    action(command)
+    action('ShowDialog()')
+
 #Send sit command with the place as the parameter
 def sit_action(place):
     command = "Sit(John, " + place + ")"
-    print('start ' + command)
-    check_for_success(command)
+    action(command)
 
 # Send a command to Camelot.
 def action(command):
@@ -26,25 +32,37 @@ def begin_game_setup():
     action('CreatePlace(QueensCastle, DiningRoom)')
     
     #Create Characters
+    #Main Character(John)
     action('CreateCharacter(John, B)')
     action('SetClothing(John, Noble)')
     action('SetHairStyle(John, Long)')
     action('SetPosition(John, QueensCastle.BackRightChair)')
+    #Queen
     action('CreateCharacter(Queen, A)')
     action('SetClothing(Queen, Queen)')
     action('SetHairStyle(Queen, Long)')
     action('SetPosition(Queen, QueensCastle.RightChair)')
     action('Sit(Queen, QueensCastle.RightChair)')
+    #GuardGallant
+    action('CreateCharacter(GuardGallant, F)')
+    action('SetClothing(GuardGallant, HeavyArmour)')
+    action('SetPosition(GuardGallant, QueensCastle.Door)')
+    action('CreateItem(GallantSword, Sword)')
+    action('SetPosition(GallantSword, GuardGallant)')
     
     #Create Items and position them
     action('CreateItem(QueensCup, Cup)')
     action('SetPosition(QueensCup, QueensCastle.DiningTable.Right)')
     action('CreateItem(JohnsCup, Cup)')
     action('SetPosition(JohnsCup, QueensCastle.DiningTable.BackRight)')
+    action('CreateItem(PythonBox, BlueBook)')
+    action('SetPosition(PythonBox, QueensCastle.Table)')
     action('ShowMenu()')
 
     #Enable Icons
     action('EnableIcon(Sit, Chair, QueensCastle.BackRightChair, Sit, true)')
+    action('EnableIcon(Python, Drink, PythonBox, Command Camelot, true)')
+    action('EnableIcon(Talk, Talk, Queen, Talk to the Queen, true)')
 # Respond to input.
 def main():
     begin_game_setup()
@@ -61,6 +79,11 @@ def main():
             action('HideNarration()')
         elif(i == 'input Sit QueensCastle.BackRightChair'):
             sit_action('QueensCastle.BackRightChair')
+        elif(i == 'input Python PythonBox'):
+            command = pyautogui.prompt("Command")
+            action(command)
+        elif(i == 'input Talk Queen'):
+            talk_action('Queen')
     
 main() #COMMENT OUT WHEN TESTING
 
