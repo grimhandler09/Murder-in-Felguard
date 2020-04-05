422787fef68c8e2bd96b12510d9d443b4d06ed5b
Travis_Conley <tdco232@uky.edu>
2020-04-05 00:06:59 -0400

    scene two completed, inventory management system edited, screen transition mouse freeze fixed, and other minor adjustments

--- src/global_game_states.py
+++ src/global_game_states.py
@@ -1,6 +1,12 @@
-player_inventory = [['Party Invitation', 'Party Invitation']]
-chest_inventory = [['Party Invitation', 'Party Invitation']]
-dirtpile_inventory = [['PlayerSword', 'PlayerSword'], ['CellDoorKey', 'CellDoorKey']]
+player_inventory = [['Party_Invitation', 'Party_Invitation']]
+chest_inventory = [['Party_Invitation', 'Party_Invitation']]
+dirtpile_inventory = [['Note_From_King', 'Note_From_King'], ['CellDoorKey', 'CellDoorKey']]
+list_of_inventories = [player_inventory, chest_inventory, dirtpile_inventory]
+acquired_CellDoorKey = True
+#scene_one_clues = []
+#scene_two_clues = []
+#scene_three_clues = []
+current_clues = []
 
 queen_death = False
 end_scene_one = False
