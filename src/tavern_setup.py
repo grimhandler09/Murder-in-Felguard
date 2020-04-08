732868b6167315a63d5a15682f5fb638843de210
mtde226 <47038060+mtde226@users.noreply.github.com>
2020-04-08 18:00:21 -0400

    Update tavern_setup.py

--- src/tavern_setup.py
+++ src/tavern_setup.py
@@ -9,21 +9,24 @@ def tavern_setup():
     action('SetPosition(Witch Carlita, Tavern.BackDoor)')
     #Noble Jeremy
     action('SetPosition(Noble Jeremy, Tavern.StoolFrontRight)')
-    action('Sit(Noble Jeremy, Tavern.StoolFrontRight)')
+    #action('Sit(Noble Jeremy, Tavern.StoolFrontRight)')
     #Noble Cecilia
     action('SetPosition(Noble Cecilia, Tavern.StoolFrontLeft)')
-    action('Sit(Noble Cecilia, Tavern.StoolFrontLeft)')
+    #action('Sit(Noble Cecilia, Tavern.StoolFrontLeft)')
     #Merchant Bert
     action('SetPosition(Merchant Bert, Tavern.StoolBackLeft)')
-    action('Sit(Merchant Bert, Tavern.StoolBackLeft)')
+    #action('Sit(Merchant Bert, Tavern.StoolBackLeft)')
     #Chamber Maid Scarlet
     action('SetPosition(Chamber Maid Scarlet, Tavern.Fireplace)')
     action('Face(Chamber Maid Scarlet, Tavern.Fireplace)')
     #Castle Grand Maester Purcell
     action('SetPosition(Maester Purcell, Tavern.Table)')
     action('Face(Maester Purcell, Tavern.Table)')
+    #Queen's Sister
+    action('SetPosition(Tiana, Tavern.Bar)')
+    action('Face(Tiana, Tavern.Fireplace)')
     #Enable Icons
-    action('EnableIcon(Sit, Chair, Tavern.StoolBackRight, Sit, true)')
+    #action('EnableIcon(Sit, Chair, Tavern.StoolBackRight, Sit, true)')
     action('EnableIcon(Enter, Exit , Tavern.Door, Leave the Tavern, true)')
-    
+    #Enable Effects
     action('EnableEffect(Tavern.Fireplace, Campfire)')
