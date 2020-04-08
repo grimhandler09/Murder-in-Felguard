cfe175fd6837b72408d0fc7c9ddb9501c5651338
mtde226 <47038060+mtde226@users.noreply.github.com>
2020-04-08 17:59:23 -0400

    Update tavern_controller.py

--- src/tavern_controller.py
+++ src/tavern_controller.py
@@ -14,8 +14,8 @@ def tavern_controller():
     action('FadeIn()')
     while global_game_states.current_scene == 'tavern' and global_game_states.accused == '':
         received = input()
-        if (received.startswith() == 'input Enter'):
-            global_game_states.current_scene = 'city'
-            global_game_states.prev_scene = 'tavern'
-        elif (received.startswith() == 'input Talk'):
+         if (received.startswith('input Enter')):
+            global_game_states.current_scene = 'scene_two_and_half'
+            global_game_states.prev_scene = 'scene_four'
+        else:
             check_master_actions(received)
